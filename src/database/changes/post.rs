1a6c4f4fd8d2add7d09d858ebac22aa25ac6a753
Michel Smola <michel.smola@farbenmeer.de>
2023-06-11 15:56:34 +0200

    add tests for post_changes permissions

--- src/database/changes/post.rs
+++ src/database/changes/post.rs
@@ -1,22 +1,16 @@
-use std::sync::Arc;
-
 use axum::extract::{Json, Path, State};
-use axum_extra::extract::CookieJar;
 use rusqlite::named_params;
 
-use crate::{app_state::AppState, auth::AuthDatabase, database::Database, error::CRRError};
+use crate::{app_state::AppState, auth::DatabasePermissions, database::Database, error::CRRError};
 
 use super::Changeset;
 
 pub(crate) async fn post_changes(
     Path(db_name): Path<String>,
-    cookies: CookieJar,
     State(state): State<AppState>,
+    permissions: DatabasePermissions,
     Json(changes): Json<Vec<Changeset>>,
 ) -> Result<(), CRRError> {
-    let permissions = AuthDatabase::open_readonly(Arc::clone(state.env()))?
-        .get_permissions(&cookies, &db_name)?;
-
     let mut db = Database::open(&state.env(), db_name, permissions)?;
 
     db.apply_changes(changes)?;
@@ -36,25 +30,28 @@ impl Database {
         let mut stmt = authorized.prepare(query)?;
 
         for changeset in changes {
-            if changeset.cid().is_none()
-                && !authorized.permissions().delete_table(changeset.table())
-            {
-                return Err(CRRError::Unauthorized(format!(
-                    "User is not authorized to delete from table {}",
-                    changeset.table()
-                )));
-            } else if changeset.col_version() == 1
-                && !authorized.permissions().insert_table(changeset.table())
-            {
-                return Err(CRRError::Unauthorized(format!(
-                    "User is not authorized to insert into table {}",
-                    changeset.table()
-                )));
-            } else if !authorized.permissions().update_table(changeset.table()) {
-                return Err(CRRError::Unauthorized(format!(
-                    "User is not authorized to update table {}",
-                    changeset.table()
-                )));
+            dbg!(&changeset);
+            if changeset.cid() == Some("__crsql_del") {
+                if !authorized.permissions().delete_table(changeset.table()) {
+                    return Err(CRRError::Unauthorized(format!(
+                        "User is not authorized to delete from table \"{}\"",
+                        changeset.table()
+                    )));
+                }
+            } else if changeset.col_version() == 1 {
+                if !authorized.permissions().insert_table(changeset.table()) {
+                    return Err(CRRError::Unauthorized(format!(
+                        "User is not authorized to insert into table \"{}\"",
+                        changeset.table()
+                    )));
+                }
+            } else {
+                if !authorized.permissions().update_table(changeset.table()) {
+                    return Err(CRRError::Unauthorized(format!(
+                        "User is not authorized to update table \"{}\"",
+                        changeset.table()
+                    )));
+                }
             }
 
             stmt.insert(named_params! {
@@ -69,5 +66,282 @@ impl Database {
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use axum::extract::{Path, State};
+
+    use super::post_changes;
+    use crate::{
+        app_state::{AppEnv, AppState},
+        auth::{DatabasePermissions, PartialPermissions},
+        database::{changes::Changeset, migrate::tests::setup_foo},
+        error::CRRError,
+    };
+
+    fn get_changes() -> [Vec<Changeset>; 3] {
+        let env = AppEnv::test_env();
+
+        setup_foo(&env);
+
+        let mut db = env.test_db();
+
+        {
+            let mut stmt = db.prepare("INSERT INTO foo (bar) VALUES (?)").unwrap();
+            stmt.insert(["a"]).unwrap();
+            stmt.insert(["b"]).unwrap();
+            stmt.insert(["c"]).unwrap();
+        }
+
+        let inserts = db
+            .all_changes()
+            .collect::<Result<Vec<Changeset>, CRRError>>()
+            .unwrap();
+
+        {
+            let mut stmt = db.prepare("UPDATE foo SET bar=?").unwrap();
+            stmt.execute(["d"]).unwrap();
+        }
+
+        let updates = db
+            .all_changes()
+            .collect::<Result<Vec<Changeset>, CRRError>>()
+            .unwrap();
+
+        {
+            let mut stmt = db.prepare("DELETE FROM foo WHERE id = ?").unwrap();
+            stmt.execute([1]).unwrap();
+        }
+
+        let deletes = db
+            .all_changes()
+            .collect::<Result<Vec<Changeset>, CRRError>>()
+            .unwrap();
+
+        [inserts, updates, deletes]
+    }
+
+    #[tokio::test]
+    async fn post_changes_with_read_permissions() {
+        let state = AppState::test_state();
+
+        setup_foo(state.env());
+
+        let [inserts, updates, deletes] = get_changes();
+
+        let permissions = DatabasePermissions::Partial {
+            database: PartialPermissions {
+                read: true,
+                insert: false,
+                update: false,
+                delete: false,
+            },
+            tables: HashMap::new(),
+        };
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(inserts.clone()),
+        )
+        .await
+        .is_err());
+
+        state.env().test_db().apply_changes(inserts).unwrap();
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(updates),
+        )
+        .await
+        .is_err());
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions,
+            axum::extract::Json(deletes),
+        )
+        .await
+        .is_err());
+    }
+
+    #[tokio::test]
+    async fn post_changes_with_insert_permissions() {
+        let state = AppState::test_state();
+        setup_foo(state.env());
+        let permissions = DatabasePermissions::Partial {
+            database: PartialPermissions {
+                read: false,
+                insert: true,
+                update: false,
+                delete: false,
+            },
+            tables: HashMap::new(),
+        };
+        let [inserts, updates, deletes] = get_changes();
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(inserts),
+        )
+        .await
+        .is_ok());
+
+        assert_eq!(
+            state
+                .env()
+                .test_db()
+                .prepare("SELECT bar FROM foo")
+                .unwrap()
+                .query_map([], |row| { row.get::<usize, String>(0) })
+                .unwrap()
+                .collect::<Result<Vec<String>, rusqlite::Error>>()
+                .unwrap(),
+            vec!["a", "b", "c"]
+        );
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(updates),
+        )
+        .await
+        .is_err());
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions,
+            axum::extract::Json(deletes),
+        )
+        .await
+        .is_err());
+    }
+
+    #[tokio::test]
+    async fn post_changes_with_update_permissions() {
+        let state = AppState::test_state();
+        setup_foo(state.env());
+        let permissions = DatabasePermissions::Partial {
+            database: PartialPermissions {
+                read: false,
+                insert: false,
+                update: true,
+                delete: false,
+            },
+            tables: HashMap::new(),
+        };
+        let [inserts, updates, deletes] = get_changes();
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(inserts.clone())
+        )
+        .await
+        .is_err());
+
+        state.env().test_db().apply_changes(inserts).unwrap();
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(updates)
+        )
+        .await
+        .is_ok());
+
+        assert_eq!(
+            state
+                .env()
+                .test_db()
+                .prepare("SELECT bar FROM foo")
+                .unwrap()
+                .query_map([], |row| { row.get::<usize, String>(0) })
+                .unwrap()
+                .collect::<Result<Vec<String>, rusqlite::Error>>()
+                .unwrap(),
+            vec!["d", "d", "d"]
+        );
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions,
+            axum::extract::Json(deletes),
+        )
+        .await
+        .is_err());
+    }
+
+    #[tokio::test]
+    async fn post_changes_with_delete_permissions() {
+        let state = AppState::test_state();
+        setup_foo(state.env());
+        let permissions = DatabasePermissions::Partial {
+            database: PartialPermissions {
+                read: false,
+                insert: false,
+                update: false,
+                delete: true,
+            },
+            tables: HashMap::new(),
+        };
+        let [inserts, updates, deletes] = get_changes();
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(inserts.clone())
+        )
+        .await
+        .is_err());
+
+        state.env().test_db().apply_changes(inserts).unwrap();
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions.clone(),
+            axum::extract::Json(updates)
+        )
+        .await
+        .is_err());
+
+        assert!(post_changes(
+            Path(AppEnv::TEST_DB_NAME.to_owned()),
+            State(state.clone()),
+            permissions,
+            axum::extract::Json(deletes),
+        )
+        .await
+        .is_ok());
+
+        assert_eq!(
+            state
+                .env()
+                .test_db()
+                .prepare("SELECT bar FROM foo")
+                .unwrap()
+                .query_map([], |row| { row.get::<usize, String>(0) })
+                .unwrap()
+                .collect::<Result<Vec<String>, rusqlite::Error>>()
+                .unwrap(),
+            vec!["b", "c"]
+        );
     }
 }
